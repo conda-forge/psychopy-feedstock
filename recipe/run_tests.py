ac4f254e22c488868c46df7cd196a1dd9aea75be
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-12-03 15:39:22 +0100

    Try to enable more Linux tests

--- recipe/run_tests.py
+++ recipe/run_tests.py
@@ -12,15 +12,11 @@ import psychopy.gui.wxgui
 import psychopy.hardware
 import psychopy.monitors
 import psychopy.preferences
+import psychopy.event
+import psychopy.visual
 
 # Disable sounddevice test on Windows, as currently we don't have a
 # soundfile package, without which this device won't work.
 if sys.platform != 'win32':
     import psychopy.sound
     assert psychopy.sound.audioLib == 'sounddevice'
-
-# Disable problematic tests on Linux.
-# See https://github.com/conda-forge/staged-recipes/pull/8645
-if sys.platform != 'linux':
-    import psychopy.event
-    import psychopy.visual
