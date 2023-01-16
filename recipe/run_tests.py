461b81c4daed943e33853f5b3ad1df6d31c1434f
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-01-16 20:45:22 +0100

    Try to fix tests and to bump to Python 3.11

--- recipe/run_tests.py
+++ recipe/run_tests.py
@@ -9,6 +9,7 @@ import psychopy.gui.wxgui
 import psychopy.hardware
 import psychopy.monitors
 import psychopy.preferences
+# import psychopy.visual
 
 # Disable sounddevice test on Windows, as currently we don't have a
 # soundfile package, without which this device won't work.
@@ -20,7 +21,6 @@ if sys.platform != 'win32':
 # See https://github.com/conda-forge/staged-recipes/pull/8645
 if sys.platform != 'linux':
     import psychopy.event
-    import psychopy.visual
     import psychopy.experiment
 
 # Skip on Linux and Windows
